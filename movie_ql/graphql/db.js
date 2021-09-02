65adef303c81f584606ee2a24eec76ff1248c421
Pangho297 <monanazzang@gmail.com>
2021-09-02 15:47:40 +0900

    part 10

--- movie_ql/graphql/db.js
+++ movie_ql/graphql/db.js
@@ -1,9 +1,22 @@
-import fetch from "node-fetch";
+import axios from "axios";
 
-const API_URL = "https://yts.am/api/v2/list_movies.json";
+export const getMovies = async (limit, rating) => {
+  let API_URL = "https://yts.mx/api/v2/list_movies.json?";
 
-export const getMovies = (limit, rating) => {
-  fetch(`${API_URL}`)
-    .then((res) => res.json())
-    .then((json) => json.data.movies);
+  if (limit > 0) {
+    API_URL += `limit=${limit}`;
+  }
+
+  if (rating > 0) {
+    API_URL += `&minimum_rating=${rating}`;
+  }
+
+  const axiosData = await axios(`${API_URL}`, {
+    params: {
+      limit,
+      rating,
+    },
+  });
+
+  return axiosData.data.data.movies;
 };
