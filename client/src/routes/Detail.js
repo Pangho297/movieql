0f6e54cadf4f98e15af13c683cf086a14924be9c
Pangho297 <monanazzang@gmail.com>
2021-09-02 18:00:43 +0900

    part 14

--- client/src/routes/Detail.js
+++ client/src/routes/Detail.js
@@ -1,5 +1,74 @@
+import styled from "styled-components";
+import { useParams } from "react-router-dom";
+import { gql, useQuery } from "@apollo/react-hooks";
+
+const GET_MOVIES = gql`
+  query getMovie($id: Int!) {
+    movie(id: $id) {
+      id
+      title
+      medium_cover_image
+      description_full
+    }
+  }
+`;
+
+// 첫번째 줄(query getMovie($id: Int))은 Apollo를 위한 줄, 변수의 type을 검사하도록 돕는다
+// 나머지는 실제 query를 서버로 전송한다
+
+const Container = styled.div`
+  height: 100vh;
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: white;
+`;
+
+const Column = styled.div`
+  margin-left: 65px;
+  width: 50%;
+`;
+
+const Title = styled.h1`
+  font-size: 35px;
+  margin-bottom: 10px;
+`;
+
+const Subtitle = styled.h4`
+  font-size: 35px;
+  margin-bottom: 10px;
+`;
+
+const Description = styled.p`
+  font-size: 28px;
+`;
+
+const Poster = styled.div`
+  width: 25%;
+  height: 60%;
+  background-color: transparent;
+  background-image: url(${(props) => props.bg});
+  background-size: cover;
+  background-position: center center;
+`;
+
 const Detail = () => {
-  return "Hello Detail";
+  const { id } = useParams();
+  const { loading, data } = useQuery(GET_MOVIES, {
+    variables: { id },
+  });
+
+  // 쿼리에 변수가 들어가게 된다면 useQuery()의 두번째 인자로 변수를 할당해주면 된다.
+  // 위의 예제처럼 variables: { args }와 같이 넣으면 된다.
+
+  if (loading) {
+    return "Loading...";
+  }
+  if (data && data.movie) {
+    return data.movie.title;
+  }
 };
 
 export default Detail;
--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -1,5 +1,77 @@
+import styled from "styled-components";
+import { useQuery } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
+
+import Movie from "../components/Movie";
+
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      description_full
+      medium_cover_image
+    }
+  }
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+const Header = styled.header`
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  height: 45vh;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+const Title = styled.h1`
+  font-size: 60px;
+  font-weight: 600;
+  margin-bottom: 20px;
+`;
+
+const Subtitle = styled.h3`
+  font-size: 35px;
+`;
+
+const Loading = styled.div`
+  font-size: 18px;
+  opacity: 0.5;
+  font-weight: 500;
+  margin-top: 10px;
+`;
+
+const Movies = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 25px;
+  width: 60%;
+  position: relative;
+  top: -50px;
+`;
+
 const Home = () => {
-  return <h1>Hello Home</h1>;
+  const { loading, data } = useQuery(GET_MOVIES);
+  return (
+    <Container>
+      <Header>
+        <Title>Apollo Movie App</Title>
+        <Subtitle>With GraphQL</Subtitle>
+      </Header>
+      {loading && <Loading>Loading...</Loading>}
+      {!loading &&
+        data.movies &&
+        data.movies.map((item) => <Movie key={item.id} id={item.id} />)}
+    </Container>
+  );
 };
 
 export default Home;
